4bf87f18feab63ac01d488b9f3471e406fa16cca
LeeWooil <lwi2765@naver.com>
2023-08-20 13:50:28 +0900

    make model

--- Make_Model.py
+++ Make_Model.py
@@ -1,4 +1,11 @@
-import torch
+from glob import glob
 
-#파이토치 버전 확인, cuda device properties 확인
-print('torch %s %s' % (torch.__version__, torch.cuda.get_device_properties(0) if torch.cuda.is_available() else 'CPU'))
+train_img_list = glob('C:\\Users\\admin\\Desktop\\wooil\\Hyper\\2D_Bounding_Box\\2D Bounding Box\\training\\images\\*.jpg')
+valid_img_list = glob('C:\\Users\\admin\\Desktop\\wooil\\Hyper\\2D_Bounding_Box\\2D Bounding Box\\validation\\images\\*.jpg')
+
+# 리스트를 txt파일로 생성
+with open('C:\\Users\\admin\\Desktop\\wooil\\Hyper\\2D_Bounding_Box\\2D Bounding Box\\train.txt', 'w') as f:
+	f.write('\n'.join(train_img_list) + '\n')
+
+with open('C:\\Users\\admin\\Desktop\\wooil\\Hyper\\2D_Bounding_Box\\2D Bounding Box\\valid.txt', 'w') as f:
+	f.write('\n'.join(valid_img_list) + '\n')
