c8555886bc606be264b59f17c2d1bcefcac210ce
LeeWooil <lwi2765@naver.com>
2023-08-20 13:50:46 +0900

    json to txt

--- Make_TXT.py
+++ Make_TXT.py
@@ -2,16 +2,33 @@ import os
 import json
 
 # JSON 파일들이 있는 디렉토리 경로
-json_dir = "c:\\Users\\admin\\Desktop\\wooil\\Hyper\\2D Bounding Box\\training\\labels"
+json_dir = "C:\\Users\\admin\\Desktop\\wooil\\Hyper\\2D_Bounding_Box\\2D Bounding Box\\training\\labels"
+
+class_mapping = {
+   'car' : 0,
+    'truck' : 1,
+    'bus' : 2,
+    'special vehicle' : 3,
+    'motorcycle' : 4,
+    'bicycle' : 5,
+    'pedestrian' : 6,
+    'traffic_sign' : 7,
+    'traffic_light' : 8
+}
+
 
 # YOLO 포맷으로 변환하는 함수
 def convert_to_yolo_format(annotation, image_resolution):
     label = annotation["Label"]
+    class_idx = class_mapping.get(label, -1)  # 클래스가 없을 경우 -1을 반환
+    if class_idx == -1:
+        return ""  # 매핑되지 않은 클래스는 빈 문자열 반환
     x_center = annotation["Coordinate"][0] / image_resolution[0]
     y_center = annotation["Coordinate"][1] / image_resolution[1]
     width = annotation["Coordinate"][2] / image_resolution[0]
     height = annotation["Coordinate"][3] / image_resolution[1]
-    return f"{label} {x_center:.6f} {y_center:.6f} {width:.6f} {height:.6f}"
+    return f"{class_idx} {x_center:.6f} {y_center:.6f} {width:.6f} {height:.6f}"
+
 
 # JSON 파일들을 처리
 for json_file_name in os.listdir(json_dir):
@@ -28,7 +45,9 @@ for json_file_name in os.listdir(json_dir):
         # Annotation 정보를 YOLO 포맷으로 변환
         for annotation in json_data["Annotation"]:
             yolo_annotation = convert_to_yolo_format(annotation, json_data["Source_Image_Info"]["Resolution"])
-            yolo_annotations.append(yolo_annotation)
+            if yolo_annotation is not None:
+                yolo_annotations.append(yolo_annotation)
+
         
         # YOLO 포맷으로 변환된 결과를 txt 파일로 저장
         output_file_name = os.path.splitext(json_file_name)[0] + ".txt"
